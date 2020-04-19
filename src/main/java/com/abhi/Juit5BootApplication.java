94edaacfacdee45d72444110d967d2cce20d42c8
abhishek <abhishek.juglan>
2020-04-19 14:17:44 -0400

    init

---
+++ src/main/java/com/abhi/Juit5BootApplication.java
@@ -0,0 +1,13 @@
+package com.abhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Juit5BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Juit5BootApplication.class, args);
+	}
+
+}
